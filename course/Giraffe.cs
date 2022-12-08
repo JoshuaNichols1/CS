96b2216e8276b7a859e57bf0371c722add1e9aea
Joshua Nichols <58839751+JoshuaNichols1@users.noreply.github.com>
2022-12-08 17:49:12 +1000

    commit 1

---
+++ course/Giraffe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = "   /|";
+            string example = "Lots of words";
+            int age = 25;
+            Console.WriteLine(example[0]);
+            Console.WriteLine(first);
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+            Console.WriteLine(age);
+
+            Console.ReadLine();
+        }
+    }
+}
